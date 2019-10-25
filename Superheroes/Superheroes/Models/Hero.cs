7ae0d70bd85eb07a27efc249fb45fbfdb77db256
Jacob Stilin <stilinj@gmail.com>
2019-10-25 09:11:12 -0500

    Initial Commit

---
+++ Superheroes/Superheroes/Models/Hero.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Superheroes.Models
+{
+    public class Hero
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string heroName { get; set; }
+
+        public string altEgo { get; set; }
+
+        public string primary { get; set; }
+
+        public string secondary { get; set; }
+
+        public string catchphrase { get; set; }
+    }
+}
